c7075160081ad928f2dd969d7a285008ce825717
Hannah Graves <hannahmgraves@gmail.com>
2022-04-12 14:41:40 -0700

    cars rendering on index page. car detail page rendering

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+	year = models.IntegerField()
+	make = models.CharField(max_length=100)
+	model = models.CharField(max_length=100)
+	color = models.CharField(max_length=100, default='color')
+
+	def __str__(self):
+		return self.model
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Car
 
 # Create your views here.
 def home(request):
@@ -9,4 +9,9 @@ def about(request):
 	return render(request, 'about.html')
 
 def cars_index(request):
-	return render(request, 'cars/index.html') #{ 'cars': cars })
+	cars = Car.objects.all()
+	return render(request, 'cars/index.html', { 'cars': cars })
+
+def cars_detail(request, car_id):
+	car = Car.objects.get(id=car_id)
+	return render(request, 'cars/detail.html', { 'car': car })

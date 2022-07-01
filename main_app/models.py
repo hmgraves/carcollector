e05a10059caf3aa3c1db88461a4c11640edaf9ad
Hannah Graves <hannahmgraves@gmail.com>
2022-07-01 07:12:54 -0700

    slight updates

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# 2-tuple to hold key and value 
 TYPES = (
 	('B', 'Brakes'),
 	('O', 'Oil change'),
